4eddbd32c0f70f5cb4369fd911d2169a5b7adcc4
nikita <nikitachumak07@gmail.com>
2026-06-11 22:12:28 +0300

    Add 17 NUnit tests.

---
+++ TestTrainer.Tests/OpenQuestionTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using TestTrainer.Models;
+
+namespace TestTrainer.Tests
+{
+    public class OpenQuestionTests
+    {
+        [Test]
+        public void CheckAnswer_ExactMatch_ReturnsTrue()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "Kyiv";
+            Assert.That(q.CheckAnswer("Kyiv"), Is.True);
+        }
+
+        [Test]
+        public void CheckAnswer_LowerCase_ReturnsTrue()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "Kyiv";
+            Assert.That(q.CheckAnswer("kyiv"), Is.True);
+        }
+
+        [Test]
+        public void CheckAnswer_UpperCase_ReturnsTrue()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "kyiv";
+            Assert.That(q.CheckAnswer("KYIV"), Is.True);
+        }
+
+        [Test]
+        public void CheckAnswer_LeadingAndTrailingSpaces_ReturnsTrue()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "Kyiv";
+            Assert.That(q.CheckAnswer("   Kyiv   "), Is.True);
+        }
+
+        [Test]
+        public void CheckAnswer_WrongAnswer_ReturnsFalse()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "Kyiv";
+            Assert.That(q.CheckAnswer("Lviv"), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_EmptyString_ReturnsFalse()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "Kyiv";
+            Assert.That(q.CheckAnswer(""), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_NullString_ReturnsFalse()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "Kyiv";
+            Assert.That(q.CheckAnswer(null), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_NumbersInString_ReturnsTrue()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "2024";
+            Assert.That(q.CheckAnswer("2024"), Is.True);
+        }
+
+        [Test]
+        public void CheckAnswer_SpecialCharacters_ReturnsTrue()
+        {
+            OpenQuestion q = new OpenQuestion();
+            q.CorrectAnswer = "C#";
+            Assert.That(q.CheckAnswer("c#"), Is.True);
+        }
+    }
+}
---
+++ TestTrainer.Tests/SingleChoiceQuestionTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using TestTrainer.Models;
+
+namespace TestTrainer.Tests
+{
+    public class SingleChoiceQuestionTests
+    {
+        [Test]
+        public void CheckAnswer_CorrectValidIndex_ReturnsTrue()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 1;
+            Assert.That(q.CheckAnswer("2"), Is.True);
+        }
+
+        [Test]
+        public void CheckAnswer_WrongValidIndex_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 1;
+            Assert.That(q.CheckAnswer("1"), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_IndexTooHigh_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 0;
+            Assert.That(q.CheckAnswer("99"), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_ZeroInput_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 0;
+            Assert.That(q.CheckAnswer("0"), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_NegativeInput_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 0;
+            Assert.That(q.CheckAnswer("-1"), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_TextInsteadOfNumber_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 0;
+            Assert.That(q.CheckAnswer("Apple"), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_EmptyString_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 0;
+            Assert.That(q.CheckAnswer(""), Is.False);
+        }
+
+        [Test]
+        public void CheckAnswer_NullInput_ReturnsFalse()
+        {
+            SingleChoiceQuestion q = new SingleChoiceQuestion();
+            q.CorrectOptionIndex = 0;
+            Assert.That(q.CheckAnswer(null), Is.False);
+        }
+    }
+}
